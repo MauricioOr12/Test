bcee1cbb33c9a46ebb58e5df7b03bb0e430fdc67
Mauricio12 <mmortiz59@misena.edu.co>
2020-10-21 08:27:34 +0900

    Nueva Modificacion Del Codigo

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 Holas Soy Tu;
 Hola Tambien Soy Tu;
 Y Aun Asi Existimos como Dos Seres Distintos;
+
+Entonces Que Hacemos;
+Nada Esperar Que El Que Este Leyendo Este Nos Recuerde;
+Porque?;
+Porque Esta Conversacion No Esta Ocurriendo Sino En La Mente Del Lector;
+Cual Lector?;
