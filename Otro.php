38369b18622a97d5d68ad57c3ec05651645961d3
Mauricio12 <mmortiz59@misena.edu.co>
2020-10-21 08:36:24 +0900

    Este Es El Segundo Cambio

--- Otro.php
+++ Otro.php
@@ -3,3 +3,8 @@ Que Quieres?;
 Ver Como Ibas;
 Entonces Que Hacemos?;
 No Se? Tu Dime;
+--------------------------------------------------------
+Entonces Hagamos Una Cosa;
+Dime;
+Hagamos Que El Usuario Nos Recuerde;
+Como Lo Hacemos;
