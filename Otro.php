ef2105b901816bed2f45d4581ba82fd4b3c8d9d4
Mauricio12 <mmortiz59@misena.edu.co>
2020-10-21 08:37:59 +0900

    El Tercer Cambio

--- Otro.php
+++ Otro.php
@@ -8,3 +8,10 @@ Entonces Hagamos Una Cosa;
 Dime;
 Hagamos Que El Usuario Nos Recuerde;
 Como Lo Hacemos;
+---------------------------------------------------------
+Pero Como Lo Hacemos?;
+Pensaba Que Me Ibas A Ayudar;
+Como?
+No Lo Se;
+Entonces Que Hacemos Aqui?;
+
